3d5a735757dcc301ee0003cb86ab4d7a0cf9aa86
kanika <kanikasahu939@gmail.com>
2024-09-19 20:43:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = ()=>{
+  // javascript logic
+  return (
+    // return only html (html is only shown in browser)
+    <div>
+      <h1>Hello Everyone</h1>
+    </div>
+  )
+}
+
+export default App;
+
+// This whole code is known as function component
